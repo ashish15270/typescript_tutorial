a6bc956963336f1f385c933953611985b6d4b3bb
ashish <a.ashish01kumar@gmail.com>
2024-09-18 14:24:27 +0000

    add numbers example

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("you code goes here...!");
+function add(num1, num2) {
+    return num1 + num2;
+}
+;
+var num1 = 10;
+var num2 = 100;
+var result = add(num1, num2);
+console.log(result);
