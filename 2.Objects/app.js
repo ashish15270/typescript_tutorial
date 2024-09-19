9c0ce92298d6d38571ed08d971a65a851a4ca62b
ashish <a.ashish01kumar@gmail.com>
2024-09-19 00:22:41 +0000

    objects

---
+++ 2.Objects/app.js
@@ -0,0 +1,5 @@
+var person = {
+    person_name: 'ashish',
+    age: 30
+};
+console.log(person.person_name + " is " + person.age + " years old");
---
+++ 2.Objects/app.ts
@@ -0,0 +1,9 @@
+const person: 
+{person_name: string, 
+  age: number
+} = {
+  person_name:'ashish',
+  age:30
+};
+
+console.log(person.person_name+ " is " + person.age + " years old");
